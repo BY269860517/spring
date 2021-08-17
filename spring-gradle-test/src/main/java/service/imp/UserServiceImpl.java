6ed0027c37571c22f5cebe96b7d9151fdbff17fa
‘Panboyang’ <269860517@qq.com>
2021-08-17 13:42:01 +0800

    spring源码

---
+++ spring-gradle-test/src/main/java/service/imp/UserServiceImpl.java
@@ -0,0 +1,15 @@
+package service.imp;
+
+import org.springframework.stereotype.Service;
+
+/***
+ * @Author 徐庶   QQ:1092002729
+ * @Slogan 致敬大师，致敬未来的你
+ */
+@Service
+public class UserServiceImpl {
+
+    public void sayHi(){
+        System.out.println("Hello Spring+");
+    }
+}
